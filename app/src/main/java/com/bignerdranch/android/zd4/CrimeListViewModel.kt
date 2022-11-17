e9763dd343ecf513313a3ef3eb86b5296520752f
Coulcloud <ultrasoykb12@mail.ru>
2022-11-17 16:55:05 +0500

    Initial commit

---
+++ app/src/main/java/com/bignerdranch/android/zd4/CrimeListViewModel.kt
@@ -0,0 +1,8 @@
+package com.bignerdranch.android.zd4
+
+import androidx.lifecycle.ViewModel
+
+class CrimeListViewModel : ViewModel() {
+    private val crimeRepository = CrimeRepository.get()
+    val crimeListLiveData = crimeRepository.getCrimes()
+}
---
+++ app/src/main/java/com/bignerdranch/android/zd4/FragmentCrimeActivity.kt
@@ -0,0 +1,12 @@
+package com.bignerdranch.android.zd4
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.bignerdranch.android.criminalintel.R
+
+class FragmentCrimeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.fragment_crime)
+    }
+}
